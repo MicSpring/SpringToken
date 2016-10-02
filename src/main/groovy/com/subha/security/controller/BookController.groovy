659ce432136cd4719eecb210a6e96bb5bc5f8968
micspring <subhafromslg123@gmail.com>
2016-10-02 18:47:33 +0530

    Spring Elastic Search Integration With ElasticSearchTemplate ElasticSearch Query Basic Tests with ElasticSearchTemplate

--- src/main/groovy/com/subha/security/controller/BookController.groovy
+++ src/main/groovy/com/subha/security/controller/BookController.groovy
@@ -9,6 +9,8 @@ import org.apache.commons.logging.LogFactory
 import org.elasticsearch.client.transport.NoNodeAvailableException
 import org.elasticsearch.common.xcontent.XContentFactory
 import org.springframework.data.domain.PageRequest
+import org.springframework.data.elasticsearch.core.query.Criteria
+import org.springframework.data.elasticsearch.core.query.CriteriaQuery
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder
 
 import static org.elasticsearch.index.query.QueryBuilders.*
@@ -75,23 +77,42 @@ class BookController {
 
 
 
-        StringQuery stringQuery = new StringQuery(query.string())
+        /*StringQuery stringQuery = new StringQuery(query.string())
         stringQuery.addIndices("book")
         stringQuery.addTypes("funcprog")
         stringQuery.setPageable(new PageRequest(0,3))
-        stringQuery.addFields("name")
+        stringQuery.addFields("name")*/
 
-        //Both the StringQuery and NativeQuery is Now working....
-        //Tomorrow's Task Intro to Criteria Query.....
+        /**
+            Both the StringQuery and NativeQuery (Hurrah!!! Nested Too) is Now working....
+         */
 
-        /*NativeSearchQueryBuilder nativeSearchQueryBuilder = new NativeSearchQueryBuilder();
-        def nativeQuery = nativeSearchQueryBuilder.withIndices("book").withTypes("funcprog").withQuery(matchAllQuery()).build()
-        */
 
-        //Criteria
+        NativeSearchQueryBuilder nativeSearchQueryBuilder = new NativeSearchQueryBuilder();
+        //def nativeQuery = nativeSearchQueryBuilder.withIndices("book").withTypes("funcprog").withQuery(matchAllQuery()).build()
 
-        def result = elasticsearchTemplate.queryForList(stringQuery,Book)/*elasticsearchTemplate.queryForPage(stringQuery,Book)*///elasticsearchTemplate.queryForList(stringQuery,Book)//elasticsearchTemplate.queryForObject(stringQuery,Book)/*elasticsearchTemplate.queryForList(nativeQuery,Book)*/
-        logger.info "######## The Result is: $result "
+        def matchQueryBuilder = matchQuery("author.name","Gordon")
+        def boolQueryBuilder = boolQuery().must(matchQueryBuilder)
+
+        def nativeSearchQuery = nativeSearchQueryBuilder.withIndices("book").withTypes("funcprog").
+                withQuery(nestedQuery("author",boolQueryBuilder)).build()
+        /**
+         * Criteria Queria do not support nested querying...
+         */
+
+        /*Criteria criteria = new Criteria("name")
+        criteria.contains("007").startsWith("Groovy2")
+        *//*Criteria criteria2 = new Criteria("author")
+        criteria2.and("author.name").startsWith("Godon2")
+        criteria.and(criteria2)*//*
+        CriteriaQuery criteriaQuery = new CriteriaQuery(criteria)
+        criteriaQuery.addIndices("book")
+        criteriaQuery.addTypes("funcprog")
+        //criteriaQuery.addFields("id")*/
+
+
+        def result = elasticsearchTemplate.queryForList(nativeSearchQuery,Book)//elasticsearchTemplate.queryForList(criteriaQuery,Book)/*elasticsearchTemplate.queryForPage(stringQuery,Book)*///elasticsearchTemplate.queryForList(stringQuery,Book)//elasticsearchTemplate.queryForObject(stringQuery,Book)/**/
+        logger.info "@@@@@@@@ The Result is: $result "
         result
     }
 
